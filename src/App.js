81c236ec717c89bde489299aa947017b67501447
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-05-18 08:42:43 +0200

    Compound Component Pattern with simple counter component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import Counter from "./Counter";
-import "./styles.css";
+import Counter from './Counter';
+import './styles.css';
 
 export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
+      {/* herkömmliche Umsetzung mit props (props explosion!) */}
+      {/* <Counter
         iconIncrease="+"
         iconDecrease="-"
         label="My NOT so flexible counter"
         hideLabel={false}
         hideIncrease={false}
         hideDecrease={false}
-      />
+      /> */}
+      <Counter>
+        <Counter.Label>My flexible counter</Counter.Label>
+        <Counter.Increase />
+        <Counter.Decrease />
+        <Counter.Count />
+      </Counter>
+      <div>
+        <Counter>
+          <Counter.Label>My flexible counter</Counter.Label>
+          <Counter.Increase />
+          <Counter.Decrease />
+          <Counter.Count />
+        </Counter>
+      </div>
     </div>
   );
 }
